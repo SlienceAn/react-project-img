75526da6492225d4c47ab1ccde11f82b95d6d2a9
MyKarken <beast964089@gmail.com>
2020-09-07 23:25:31 +0800

    新增Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Main from './components/Main'
 function App() {
-  return ( 
+  return (
     <div>
-      <h1>778888</h1>
+      <Main />
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,24 @@
-function Component() {
-  return (
-    <div>
-      
-    </div>
-  );
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import ProjectCard from './ProjectCard'
+function Main(props) {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src="/logo.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />
+      專案經歷
+    </Navbar.Brand>
+            </Navbar>
+            <ProjectCard />
+        </div>
+    );
 }
+
+export default Main;
