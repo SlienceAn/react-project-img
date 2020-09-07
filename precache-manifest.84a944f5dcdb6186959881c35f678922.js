a5422f7b52045ed1d3f44b26fb12cf89efd2e832
MyKarken <beast964089@gmail.com>
2020-09-07 16:29:40 +0800

    Updates

---
+++ precache-manifest.84a944f5dcdb6186959881c35f678922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb61327751861778ad637f3715a13966",
+    "url": "/react-project-img/index.html"
+  },
+  {
+    "revision": "e5fd3f6231c725b4e4e3",
+    "url": "/react-project-img/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a451339554511027d2e",
+    "url": "/react-project-img/static/js/2.d96bd013.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project-img/static/js/2.d96bd013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fd3f6231c725b4e4e3",
+    "url": "/react-project-img/static/js/main.e14928a0.chunk.js"
+  },
+  {
+    "revision": "b032c62c3bda59c7f217",
+    "url": "/react-project-img/static/js/runtime-main.8bf11dc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-project-img/static/media/logo.5d5d9eef.svg"
+  }
+]);
